90e7cc0ef7f68049f36efe74ff3c9ecca8ca3ead
kim_haaga <kim.haaga@live.se>
2021-04-27 14:46:51 +0200

    Add Menu class component, change App.js to .jsx and made it a class

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import Menu from './components/Menu'
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <h1>Slowfood</h1>
+        <Menu />
+      </>
+    )
+  }
+}
+
+export default App;
+
