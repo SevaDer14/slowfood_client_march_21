cc1df60d17e2c21d81e0bde8ca5ba735b4880ee1
sevader14 <vsevolod.deriushkin@gmail.com>
2021-04-29 13:10:44 +0200

    to pull from Arun

--- src/modules/positionData.js
+++ src/modules/positionData.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const getPositions = async () => {
   const response = await axios.get("/menu")
-  return response.data.positions;
+  return response.data.menu_items;
 };
 
 export { getPositions }
