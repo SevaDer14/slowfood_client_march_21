c4b2c90a2caf4d200b3d78cbf12f04f7f9013be6
sevader14 <vsevolod.deriushkin@gmail.com>
2021-04-30 19:20:59 +0200

    adds conditions to handle unssuccessful log-in

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -3,73 +3,90 @@ import React, { Component } from "react";
 
 class LogIn extends Component {
   state = {
-    renderForm: false,    
+    renderForm: false,
   };
 
   loginUser = async (event) => {
-    event.preventDefault()
-    
+    event.preventDefault();
+
     let credentials = {
       email: event.target.email.value,
-      password: event.target.password.value
-    }
+      password: event.target.password.value,
+    };
     try {
-    let response = await axios.get("/users", credentials)
-    const userCredentials = {
-      uid: response.headers['uid'],
-      client: response.headers['client'],
-      acces_token: response.headers['access-token'],
-      expiry: response.headers['expiry'],
-      token_type: "Bearer"
-    }
+      let response = await axios.get("/users", credentials);
+      const userCredentials = {
+        uid: response.headers["uid"],
+        client: response.headers["client"],
+        acces_token: response.headers["access-token"],
+        expiry: response.headers["expiry"],
+        token_type: "Bearer",
+      };
 
-    localStorage.setItem('userData', JSON.stringify(userCredentials))
-    this.setState({
-      message: 'You logged in successfully',
-      renderForm: false
-    })  
-    this.props.authStatus()  
+      localStorage.setItem("userData", JSON.stringify(userCredentials));
+      this.setState({
+        message: "You logged in successfully",
+        renderForm: false,
+      });
+      this.props.authStatus();
     } catch (error) {
       this.setState({
-        message: 'Wrong email or password',
+        message: "Wrong email or password",
         renderForm: false,
-        keepLogin: true
-      })
+        keepLogin: true,
+      });
     }
-  }
+  };
 
   render() {
-    const {renderForm, message, login} = this.state    
+    const { renderForm, message, keepLogin } = this.state;
     return (
       <div>
-        {renderForm ? (
-          <form onSubmit={(event) => this.loginUser(event)}>
-            <input
-              data-cy="email-input"
-              type="email"
-              name="email"
-              placeholder="Your mail"
-            />
-            <input
-              data-cy="password-input"
-              type="password"
-              name="password"
-              placeholder="Your password"
-            />
-            <button type="submit" data-cy="submit-log-in">
-              Submit
+        {
+        (renderForm) 
+        ? (
+            <form onSubmit={(event) => this.loginUser(event)}>
+              <input
+                data-cy="email-input"
+                type="email"
+                name="email"
+                placeholder="Your mail"
+              />
+              <input
+                data-cy="password-input"
+                type="password"
+                name="password"
+                placeholder="Your password"
+              />
+              <button type="submit" data-cy="submit-log-in">
+                Submit
+              </button>
+            </form>
+          ) 
+        : !(message)
+        ?
+          (
+            <button
+              data-cy="log-in-button"
+              onClick={() => this.setState({ renderForm: true })}
+            >
+              Log in
             </button>
-          </form>
-        ) : message ? (
-                
-          <div data-cy="message">{message}</div>
-        ) : (
-          <button
-          data-cy="log-in-button"
-          onClick={() => this.setState({ renderForm: true })}>
-            Log in
-          </button>
-        )}
+          )
+        : (keepLogin)
+        ? (
+          <>
+            <div data-cy="message">{message}</div>
+            <button
+                data-cy="log-in-button"
+                onClick={() => this.setState({ renderForm: true })}
+              >
+                Log in
+            </button>
+          </>
+        ) 
+        : (<div data-cy="message">{message}</div>) 
+        }
       </div>
     );
   }
