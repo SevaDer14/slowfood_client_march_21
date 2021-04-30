0aebcf8e5b9c4efc1e217c1524fe96c7bcee9eb5
sevader14 <vsevolod.deriushkin@gmail.com>
2021-04-30 11:17:41 +0200

    adds tests for unsuccessful log-in with wrong email and wrong password also adds check for order-button to appear after log-in

--- cypress/integration/userCanLogIn.feature.js
+++ cypress/integration/userCanLogIn.feature.js
@@ -9,14 +9,39 @@ describe('User can log in', () => {
     cy.visit("/")
   })
 
-  describe('successfully', () => {
-    it('with email and password', () => {
+  
+  describe('successfully with correct email and password', () => {
+    
+    it('is expected for order-burron to appear after successful log-in', () => {
+      cy.get('[data-cy="order-button"]').should('not.exist')
       cy.get('[data-cy="log-in-button"]').click()
       cy.get('[data-cy="email-input"]').type("example@example.com")
       cy.get('[data-cy="password-input"]').type("password")
       cy.get('[data-cy="submit-log-in"]').click()
       cy.get('[data-cy="success-message"]').should('contain','You logged in successfully')
       cy.get('[data-cy="order-button"]').should('be.visible')
+    });
+  })
+
+  describe('unsuccessfully with wrong email', () => {    
+    
+    it('is expected to show error message', () => {      
+      cy.get('[data-cy="log-in-button"]').click()
+      cy.get('[data-cy="email-input"]').type("wrong@example.com")
+      cy.get('[data-cy="password-input"]').type("password")
+      cy.get('[data-cy="submit-log-in"]').click()
+      cy.get('[data-cy="success-message"]').should('contain','Wrong email or password')      
+    });
+  })
+
+  describe('unsuccessfully with wrong password', () => {    
+    
+    it('is expected to show error message', () => {      
+      cy.get('[data-cy="log-in-button"]').click()
+      cy.get('[data-cy="email-input"]').type("example@example.com")
+      cy.get('[data-cy="password-input"]').type("wrongPassword")
+      cy.get('[data-cy="submit-log-in"]').click()
+      cy.get('[data-cy="success-message"]').should('contain','Wrong email or password')      
     });
   })
 })
