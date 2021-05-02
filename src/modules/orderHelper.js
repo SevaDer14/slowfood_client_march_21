d288b22391096c5c1b57935d1df14cef103cf314
sevader14 <vsevolod.deriushkin@gmail.com>
2021-05-02 20:19:22 +0200

    fix mistakes in paths in API calls

--- src/modules/orderHelper.js
+++ src/modules/orderHelper.js
@@ -4,7 +4,7 @@ const authHeaders = JSON.parse(localStorage.getItem('userData'))
 
 const createOrder = async (item_id) => {
   let response = await axios.post(
-    './orders',
+    'http://localhost:3000/api/orders',
      {item_id: item_id}, 
      {headers: authHeaders}) 
   return response.data
@@ -12,19 +12,19 @@ const createOrder = async (item_id) => {
 
 const updateOrder = async (item_id, order_id) => {
   let response = await axios.put(
-    `./orders/${order_id}`,
+    `http://localhost:3000/api/orders/${order_id}`,
      {item_id: item_id}, 
      {headers: authHeaders}) 
   return response.data.body
 }
 
 const getOrder = async (order_id) => {
-  const response = await axios.get(`/orders/${order_id}`, {headers: authHeaders})
+  const response = await axios.get(`http://localhost:3000/api/orders/${order_id}`, {headers: authHeaders})
   return response.data.order.items;
 };
 
 const closeOrder = async (order_id) => {
-  const response = await axios.put(`/orders/${order_id}`, {params: {finalized: true}}, {headers: authHeaders})
+  const response = await axios.put(`http://localhost:3000/api/orders/${order_id}`, {params: {finalized: true}}, {headers: authHeaders})
   return response.data
 }
 
