606f57e68f5911b568383953d35acecd12c8388c
luke <lukeperrin27@gmail.com>
2021-05-04 09:47:21 +0200

    debugs calculate total price function updates key in map function for order items in view order

--- src/components/ViewOrder.jsx
+++ src/components/ViewOrder.jsx
@@ -1,54 +1,54 @@
-import React, { Component } from 'react'
-import {getOrder, closeOrder} from '../modules/orderHelper'
-import {Item, Container, Header, Button} from 'semantic-ui-react'
+import React, { Component } from "react";
+import { getOrder, closeOrder } from "../modules/orderHelper";
+import { Item, Container, Header, Button } from "semantic-ui-react";
 
 class ViewOrder extends Component {
-
   state = {
     orderData: [],
-    totalPrice: 0,    
   };
 
   componentDidMount() {
-    this.getOrderData();    
-  };
+    this.getOrderData();
+    this.calculateTotalPrice();
+  }
 
   getOrderData = async () => {
     let result = await getOrder(this.props.orderId);
-    
+
     this.setState({ orderData: result });
-    this.setState({totalPrice: this.calculateTotalPrice()})    
+    
+    this.setState({ orderTotalPrice: this.calculateTotalPrice() });
   };
 
   calculateTotalPrice = () => {
-    let totalPrice = 0    
-    this.state.orderData.map((item) => {      
-       totalPrice += item.price
-      debugger
-      return totalPrice      
-    })    
-  }
+    let totalPrice = 0;
+    
+    this.state.orderData.map((item) => {
+      totalPrice += item.price;
+      return totalPrice
+    });
+    this.setState({ totalPrice: totalPrice });
+  };
 
   finalizeOrder = async () => {
-    let result = await closeOrder(this.props.orderId)
+    let result = await closeOrder(this.props.orderId);
     this.setPickUpTime(result.order.updated_at);
-  }
+  };
 
   setPickUpTime = (timeOfOrder) => {
-    let timestamp =`${timeOfOrder.slice(11, 16)}`
-    let [hh, mm] = timestamp.split(':').map(s=>parseInt(s, 10));
+    let timestamp = `${timeOfOrder.slice(11, 16)}`;
+    let [hh, mm] = timestamp.split(":").map((s) => parseInt(s, 10));
     const d = new Date();
-    d.setHours(hh+2);
+    d.setHours(hh + 2);
     d.setMinutes(mm);
-    const pickUpTime = new Date(d.getTime() + 30 * 60 * 1000)    
-    this.setState({pickUpTime: `${pickUpTime.toString().slice(16, 21)}`})
-  }
-   
-  
+    const pickUpTime = new Date(d.getTime() + 30 * 60 * 1000);
+    this.setState({ pickUpTime: `${pickUpTime.toString().slice(16, 21)}` });
+  };
+
   render() {
     let orderItems = this.state.orderData.map((item, i) => {
       return (
-        <Item data-cy={`item-${i}`}>
+        <Item key={i} data-cy={`item-${i}`}>
           <Item.Content>
             <Item.Header data-cy="title">{item.title}</Item.Header>
             <Item.Extra data-cy="price">{item.price}Kr</Item.Extra>
@@ -58,22 +58,27 @@ class ViewOrder extends Component {
     });
     return (
       <>
-      <Header data-cy="menu-category-header">{this.props.tab}</Header>
-      <Container data-cy='order-list'>
-        {orderItems}
-      </Container>
-      <p data-cy='total-price'>Total price: {this.state.totalPrice}Kr</p>
-      <Button onClick={() => {this.finalizeOrder()}} data-cy="finalize-order-button">Confirm Order</Button>
-      {(this.state.pickUpTime) 
-      && (
-        <>
-        <p data-cy="order-confirmation-message">Thank you for your order</p>
-        <p data-cy="order-confirmation-ready-time">Your order will be ready at {this.state.pickUpTime}</p>
-        </>
-      )
-      }
+        <Header data-cy="menu-category-header">{this.props.tab}</Header>
+        <Container data-cy="order-list">{orderItems}</Container>
+        <p data-cy="total-price">Total price: {this.state.totalPrice}Kr</p>
+        <Button
+          onClick={() => {
+            this.finalizeOrder();
+          }}
+          data-cy="finalize-order-button"
+        >
+          Confirm Order
+        </Button>
+        {this.state.pickUpTime && (
+          <>
+            <p data-cy="order-confirmation-message">Thank you for your order</p>
+            <p data-cy="order-confirmation-ready-time">
+              Your order will be ready at {this.state.pickUpTime}
+            </p>
+          </>
+        )}
       </>
-    )
+    );
   }
 }
-export default ViewOrder
+export default ViewOrder;
